0be23860c59eecd61153a05a812d177528f8f9fa
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2024-06-08 13:56:09 +0530

    initial commit

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: "chat-app-eff7e.firebaseapp.com",
+    projectId: "chat-app-eff7e",
+    storageBucket: "chat-app-eff7e.appspot.com",
+    messagingSenderId: "768036085164",
+    appId: "1:768036085164:web:5a4c906b083977aecb2e09"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
+export const storage = getStorage()
